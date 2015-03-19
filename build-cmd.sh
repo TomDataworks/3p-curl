29e8693ab51235ff83d2f77ee402449af7291200
Cinder <cinder@sdf.org>
2015-03-19 12:39:41 -0600

    Bump version number to 7.41.0

--- build-cmd.sh
+++ build-cmd.sh
@@ -15,7 +15,7 @@ if [ "$OSTYPE" = "cygwin" ] ; then
     export AUTOBUILD="$(cygpath -u $AUTOBUILD)"
 fi
 
-CURL_VERSION="7.38"
+CURL_VERSION="7.41.0"
 CURL_SOURCE_DIR="curl"
 
 # load autbuild provided shell functions and variables
